42749309887f9724eee25f5fa391855bdca7aa11
Erik C. Thauvin <erik@thauvin.net>
2024-01-31 15:58:22 -0800

    Bumped bld to 1.8.0

--- examples/bld/src/bld/java/net/thauvin/erik/pinboard/samples/ExampleBuild.java
+++ examples/bld/src/bld/java/net/thauvin/erik/pinboard/samples/ExampleBuild.java
@@ -25,7 +25,7 @@ public class ExampleBuild extends BaseProject {
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, SONATYPE_SNAPSHOTS_LEGACY);
 
         scope(compile)
-                .include(dependency("net.thauvin.erik", "pinboard-poster", version(1, 1, 1)));
+                .include(dependency("net.thauvin.erik", "pinboard-poster", version(1, 1, 2, "SNAPSHOT")));
     }
 
     public static void main(String[] args) {
