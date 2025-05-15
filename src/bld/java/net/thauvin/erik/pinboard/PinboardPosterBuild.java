7d77cab5845ae9dbe5233bd98e7b8e34de0c5d28
Erik C. Thauvin <erik@thauvin.net>
2025-05-14 22:52:18 -0700

    Bump Kotlin to version 2.1.21

--- src/bld/java/net/thauvin/erik/pinboard/PinboardPosterBuild.java
+++ src/bld/java/net/thauvin/erik/pinboard/PinboardPosterBuild.java
@@ -73,7 +73,7 @@ public class PinboardPosterBuild extends Project {
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL);
 
         final var okHttp = version(4, 12, 0);
-        final var kotlin = version(2, 1, 20);
+        final var kotlin = version(2, 1, 21);
         scope(compile)
                 // Kotlin
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib", kotlin))
